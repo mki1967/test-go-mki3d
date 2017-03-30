61d4a49167093c3ff9f7d90d53b1c264eca93edc
Marcin Kik <mki1967@gmail.com>
2017-03-30 09:45:07 +0200

    some code in shaders.go

--- shaders.go
+++ shaders.go
@@ -1,17 +1,129 @@
+
 package main
+
+
+import (
+	"fmt"
+	"strings"
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
 
 var vertexShader = `
 #version 330
+
+/* attributes */
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec3 color;
+
+/* uniforms */
+uniform mat4 model; 
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec4 light;
+uniform float ambient; 
+ 
+/* output to fragment shader */
+out vec4 vColor;
+
 void main() {
-    gl_Position = vec4(position, 1);
+    /* compute shaded color */
+    vec4 modelNormal=model*vec4(normal, 1);
+    float shade= abs( dot( modelNormal, light ) ); 
+    vColor= ambient+(1.0-ambient)*shade;
+    /* compute projected position */
+    gl_Position = projection*view*model*vec4(position, 1);
 }
 ` + "\x00"
 
 var fragmentShader = `
 #version 330
+
+/* input from vertex shader */
+in vec4 vColor;
+
+/* fragment color output */
 out vec4 outputColor;
+
 void main() {
-    outputColor = vec4(1.0, 0.0, 1.0, 1.0);
+    outputColor = vColor ;
 }
 ` + "\x00"
+
+// from https://github.com/go-gl/examples/blob/master/gl41core-cube/cube.go 
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+
+// from https://github.com/go-gl/examples/blob/master/gl41core-cube/cube.go 
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
+
+
+type Mki3dShader struct {
+	// program Id
+	ProgramId uint32
+	// locations of attributes
+	PositionsAttr uint32
+	NormalsAttr uint32
+	ColorsAttr uint32
+        // locations of uniforms	
+	ProjectionUni uint32
+	ViewUni uint32
+	ModelUni  uint32
+	LightUni uint32
+}
+
